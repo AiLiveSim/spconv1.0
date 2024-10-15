9854aec37c6bb0e1f59dd920e81fb3d354bd28dc
Faiz <faiz@ailivesim.com>
2024-10-15 10:55:45 +0300

    push changes

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 import os
-import re
 import sys
-import platform
 import subprocess
-import torch
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
-from setuptools.command.sdist import sdist
-from distutils.version import LooseVersion
-
-from pathlib import Path
 
 # Configure and build C++/CUDA using CMAKE
 class CMakeBuild(build_ext):
@@ -18,20 +11,19 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        print(extdir)
+        build_directory = os.path.abspath(self.build_temp)
+        install_directory = os.path.abspath(os.path.join(self.build_lib, ext.name))
+        os.makedirs(build_directory, exist_ok=True)
+        os.makedirs(install_directory, exist_ok=True)
         cmake_args = [
-            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}/spconv/lib',
-            f'-DCMAKE_INSTALL_PREFIX={extdir}/spconv',
+            f'-DCMAKE_INSTALL_PREFIX={install_directory}',
             f'-DPYTHON_EXECUTABLE={sys.executable}',
         ]
         build_args = ['--config', 'Release', '-j8']
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--install', '.'], cwd=self.build_temp)
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
